614af3265291bd693f8c119a95cc055527c97707
YaGRRusso <yagrrusso@gmail.com>
2023-08-23 23:41:35 -0300

    chore: folders reorder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { VehiclesModule } from './vehicles/vehicles.module'
-import { PrismaModule } from './prisma/prisma.module'
+import { PrismaModule } from '@database/prisma.module'
 
 @Module({
   imports: [PrismaModule, UsersModule, VehiclesModule],
